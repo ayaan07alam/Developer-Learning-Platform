e7db3d3a6be5d93eb3dc1cb60c848ef09660033e
Ayaan Alam <ayaanalam78670@gmail.com>
2025-12-31 02:48:05 +0530

    fix: allow unknown properties in FAQDTO deserialization

--- backend/src/main/java/com/blog/backend/dto/FAQDTO.java
+++ backend/src/main/java/com/blog/backend/dto/FAQDTO.java
@@ -1,8 +1,10 @@
 package com.blog.backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class FAQDTO {
     private String question;
     private String answer;
